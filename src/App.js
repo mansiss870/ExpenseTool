c41532d090b1ba1338e588c87dd29f3ac7f52528
Mansi Sharma <mansiss870@gmail.com>
2022-08-07 12:28:24 +0530

    app completed

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import './App.css';
 import NewExpense from './components/newExpense/NewExpense';
 
 const DUMMY_DATA = [
-  {id : "e1", date : new Date(2022, 2, 2), title : "Mugs", amount: "$ 22"},
-  {id : "e2", date : new Date(2022, 2, 4), title : "Notebooks", amount: "$ 45"},
-  {id : "e3", date : new Date(2021, 2, 23), title : "Lamp", amount: "$ 26"},
-  {id : "e4", date : new Date(2020, 2, 25), title : "Sugar", amount: "$ 25"},
+  {id : "e1", date : new Date(2022, 2, 2), title : "Mugs", amount: "22"},
+  {id : "e2", date : new Date(2022, 1, 4), title : "Notebooks", amount: "45"},
+  {id : "e3", date : new Date(2021, 2, 23), title : "Lamp", amount: "26"},
+  {id : "e4", date : new Date(2020, 2, 25), title : "Sugar", amount: "25"},
 ];
 
 const App = () => {
